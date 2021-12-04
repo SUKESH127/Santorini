9c1d0d33603ad98b1e3c522bb5d5ffe62221f5c9
ColeKaufmann <cole.kaufmann@yale.edu>
2021-12-04 15:06:58 -0500

    Update board.py

--- board.py
+++ board.py
@@ -4,37 +4,48 @@
 class Square:
 
     def __init__(self):
-        self.level = 0
-        self.worker is None
+        self._level = 0
+        self._worker = ' '
     
     def has_worker(self):
-        if self.worker is None:
+        if self._worker == ' ':
             return False
         return True
     
     def assign_worker(self, w):
-        self.worker = w
+        self._worker = w
     
     def can_build(self):
-        return self.level != 4
+        return self._level != 4   
 
     def build(self):
-        self.level += 1
-        
+        self._level += 1
+    
+    def __str__(self):
+        return f'{self._level}{self._worker}'
 
 
 class Board:
 
     def __init__(self):
-        self._grid = []
+        self.grid = []
         for i in range(5):
             row = []
             for j in range(5):
                 row.append(Square())
-            grid.append(row)
+            self.grid.append(row)
+        
+        # assign player 1's workers
+        self.grid[3][1].assign_worker('A')
+        self.grid[1][3].assign_worker('B')
 
-        # assign player 1 workers
-        self._grid[2][2].assign_worker()
+        # assign player 2's workers
+        self.grid[1][1].assign_worker('Y')
+        self.grid[3][3].assign_worker('Z')
 
-        # assign player 2 workers
-
+    def __str__(self):
+        print("+--+--+--+--+--+")
+        for i in range(5):
+            for j in range(5):
+                print(f'|{self.grid[i][j]}', end='')
+            print("|\n+--+--+--+--+--+")
