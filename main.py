ca0cbe1e51d187aa01914f40eac66a3d0422a40f
Sukesh Ram <sukeshram7245@gmail.com>
2021-12-04 15:22:56 -0500

    Update main.py
    adding command line input processing check

--- main.py
+++ main.py
@@ -4,20 +4,64 @@ import sys
 from board import Board
 from player import Player
 
+import sys
+class ProcessInput():
+    def __init__(self, args):
+        self.args = args[1:]
+
+        self.whitePlayer = "human"
+        self.bluePlayer = "human"
+        self.enableUndoRedo = "off"
+        self.enableScore = "off"
+
+        if not self.validateSetup():
+            print("Invalid setup")
+            sys.exit(1)
+        else:
+            Manager()._run()
+    
+    def validateSetup(self):
+        return self.checkWhitePlayer() and self.checkBluePlayer() and self.checkEnableUndoRedo() and self.checkEnableScore()
+    
+    def checkWhitePlayer(self):
+        if self.args[0] not in {"human", "heuristic", "random"}:
+            return False
+        else:
+            self.whitePlayer = self.args[0]
+            return True
+    
+    def checkBluePlayer(self):
+        if self.args[1] not in {"human", "heuristic", "random"}:
+            return False
+        else:
+            self.bluePlayer = self.args[1]
+            return True
+    
+    def checkEnableUndoRedo(self):
+        if self.args[2] not in {"on", "off"}:
+            return False
+        else:
+            self.enableUndoRedo = self.args[2]
+            return True
+    
+    def checkEnableScore(self):
+        if self.args[3] not in {"on", "off"}:
+            return False
+        else:
+            self.enableScore = self.args[3]
+            return True
+
+
 class Manager:
-
-
     def __init__(self):
         self._board = Board()
         self._p1 = Player()
         self._p2 = Player()
 
-
     def run(self):
+        
         while True:
             break
 
-
-
 if __name__ == "__main__":
-    Manager()._run()
+    ProcessInput(sys.argv)
